1b98107de9a7505b740fe8300ad2d1c8dbd1eadb
František Ficker <fickerf24@spstrutnov.cz>
2026-03-03 10:36:41 +0100

    Register a uživatelský profil
    Pomocí umělé inteligence jsem vytvořil register jako samostatné view a také uživatelský profil jako samostatné view Input funguje tak jak má

---
+++ web_aplikace/web_aplikace/Controllers/UserControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using web_aplikace.Models;
+
+namespace web_aplikace.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["FirstName"] = user.FirstName;
+                TempData["LastName"] = user.LastName;
+                TempData["Password"] = user.Password;
+
+                return RedirectToAction("Profil");
+            }
+            return View(user);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Profil()
+        {
+            ViewBag.FirstName = TempData["FirstName"]?.ToString() ?? "Neuvedeno";
+            ViewBag.LastName = TempData["LastName"]?.ToString() ?? "Neuvedeno";
+            ViewBag.Password = TempData["Password"]?.ToString() ?? "Neuvedeno";
+
+            return View("UserProfil");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
